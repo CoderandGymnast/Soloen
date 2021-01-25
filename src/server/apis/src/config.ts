075e796fe164af69c57aa1a96b42cad192acfd3a
Long Vu <redralogro@gmail.com>
2021-01-25 19:27:36 +0700

    Revert "Add user's relation"
    This reverts commit 182603fd31f3eff33c96fe6f2c8411de6053531b.

--- src/server/apis/src/config.ts
+++ src/server/apis/src/config.ts
@@ -14,8 +14,7 @@ const config = {
             wallet: process.env.DB_TABLE_WALLET || "wallet",
             address: process.env.DB_TABLE_ADDRESS || "address",
             progress: process.env.DB_PROGRESS || "progress",
-            contract: process.env.DB_CONTRACT || "contract",
-            user: process.env.DB_USER || "user"
+            contract: process.env.DB_CONTRACT || "contract"
         }   
     },
     nodes: {
